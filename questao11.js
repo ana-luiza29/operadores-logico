e4d36f244b593bff199e65f0bb99dad402f4326e
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-27 11:20:13 -0400

    finalizado questao11

---
+++ questao11.js
@@ -0,0 +1,9 @@
+/*
+11. Explique a diferença entre os operadores && e || em JavaScript. 
+ o && indica que esta é a expressão o || diz que é um ou outro 
+ Resposta= o operador && exige que todas as 
+ condições sejam verdadeiras para que o resultado seja verdadeiro, enquanto o operador || 
+ requer que pelo menos uma condição seja verdadeira para que o resultado seja verdadeiro. 
+ Além disso, ambos os operadores suportam a lógica de curto-circuito, o que significa que 
+ a avaliação para de imediato assim que o resultado final for determinado.
+ */
