04907225de9390c6b41c337497b564b7fa916fad
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-03 08:50:06 -0400

    finalizada a questao 26

---
+++ questao26.js
@@ -0,0 +1,5 @@
+/*
+
+26. Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript. 
+ ao seguir os parenteses antes da expressao conseguimos saber o resultado e ter certeza da resposta*/
